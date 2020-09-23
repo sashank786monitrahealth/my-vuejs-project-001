de639aed950c29a83bd9e936fe5c111daa567174
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-09-23 08:32:02 +0530

    using v-bind to bind to href

---
+++ 004-v-bind/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data(){
+        return {
+          courseGoal:"finish the course and Learn Vue.",
+          vueLink:"https://v3.vuejs.org/"
+        };
+    }
+
+    ,
+    methods:function(){
+
+    }
+}) ;
+
+
+app.mount("#user-goal");
