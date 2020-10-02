4359dc38a38d78beba14f5ed2690fe4b4b3d069a
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-03 02:33:41 +0530

    on-click call methods

--- 011-events-and-methods/app.js
+++ 011-events-and-methods/app.js
@@ -1,9 +1,21 @@
+function add(){
+  counter++;
+}
+
 const app = Vue.createApp({
-  data() {
+  data: function() {
     return {
       counter: 0,
     };
   },
+  methods:{
+    add(){
+      this.counter++;
+    },
+    sub(){
+      this.counter--;
+    }
+  }
 });
 
 app.mount('#events');
