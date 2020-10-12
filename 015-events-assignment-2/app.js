f4f0620ef3e43d1b9b35c0a9a10146c429585ab7
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-10-12 16:16:39 +0530

    final

--- 015-events-assignment-2/app.js
+++ 015-events-assignment-2/app.js
@@ -1,12 +1,23 @@
 const app = Vue.createApp({
 data(){
 return {
-
+    username:"",
+    username2:""
 };
 },
 methods:{
  showAlert(){
      alert("you pressed a button.")
+ },
+ showOutput(event){
+     //console.log(event)
+    this.username = event.target.value;
+
+ },
+ showOutput2(event){
+     console.log(event)
+    this.username2 = event.target.value;
+
  }
 }
 });
